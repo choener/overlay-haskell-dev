4e371230e8a62ee6590896ed94feaeacc531186d
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-19 03:15:20 +0100

    accidentally overwrote all vim plugins

--- default.nix
+++ default.nix
@@ -2,15 +2,17 @@ self: super: {
 
 hsDevFunctions = ((import ./hsdevfunctions) self super).hsDevFunctions;
 
-vimPlugins.ghcid = self.vimUtils.buildVimPlugin {
-  name = "vim-ghcid";
-  src = self.fetchFromGitHub {
-    owner = "ndmitchell";
-    repo = "ghcid";
-    rev = "b38d16bae64e036063650279d2811d13368b5b55";
-    sha256 = "083d12cp4cldlmc0fqxj3j4xpa9azgbbd2392s7b9pp6rjk0gcjn";
-  };
-  sourceRoot = "source/plugins/nvim";
+vimPlugins = super.vimPlugins // {
+  ghcid = self.vimUtils.buildVimPlugin {
+    name = "vim-ghcid";
+    src = self.fetchFromGitHub {
+      owner = "ndmitchell";
+      repo = "ghcid";
+      rev = "50eb9908d6da67e5e4b4e6db2828b81bd7468ae3";
+      sha256 = "0wbp44gvb0rljqjr58slsqzzzy9knn9lfl4xdhwwczw7dvz594vb";
+    };
+    sourceRoot = "source/plugins/nvim";
+  }; 
 };
 
 }
