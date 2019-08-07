1dd506336bc23a72ba5bc18a1948ec6b3126ceeb
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-08-07 19:00:28 +0200

    added niv

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
-self: super: {
+self: super:
+
+{
 
 hsDevFunctions = ((import ./hsdevfunctions) self super).hsDevFunctions;
 
@@ -14,7 +16,18 @@ vimPlugins = super.vimPlugins // {
       sha256 = "0wbp44gvb0rljqjr58slsqzzzy9knn9lfl4xdhwwczw7dvz594vb";
     };
     sourceRoot = "source/plugins/nvim";
-  }; 
+  };
 };
 
+# easy dependency management with niv (not just for Haskell stuff)
+# This is a set, niv is under @niv.niv@
+
+niv = import (self.fetchFromGitHub {
+    owner  = "nmattia";
+    repo   = "niv";
+    rev    = "8b7b70465c130d8d7a98fba1396ad1481daee518";
+    sha256 = "0fgdrxn2vzpnzr6pxaiyn5zzbd812c6f7xjjhfir0kpzamjnxwwl";
+  }) {};
+
 }
+
